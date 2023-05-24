0f0092d62385a1bb003d2eeef64b7e5ebb29b0db
Marcin Grzebyk <marcin15g@gmail.com>
2023-05-24 15:08:49 +0200

    firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'workout-trackerr',
+    appId: '1:699163108086:web:6c2da3cec253200a8e42d7',
+    storageBucket: 'workout-trackerr.appspot.com',
+    apiKey: 'AIzaSyClkmQdkxg3XYou3xzQDNhms8hzxSI-WjQ',
+    authDomain: 'workout-trackerr.firebaseapp.com',
+    messagingSenderId: '699163108086',
+    measurementId: 'G-RPPX2P33E5',
+  },};
